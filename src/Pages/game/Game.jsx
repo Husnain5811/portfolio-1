ec9fd41f7bc419238f1e0677184c4be2dc1cfdda
Husnain5811 <husnainrazaghulamraza@gmail.com>
2026-03-09 05:28:57 -0700

    fix cardgame width

--- src/Pages/game/Game.jsx
+++ src/Pages/game/Game.jsx
@@ -4,7 +4,7 @@ const Game = () => {
   return (
     <>
       <div className='w-full h-screen flex justify-center items-center p-20 bg-yellow-400'>
-        <Cardgame className='w-full h-full max-w-6xl' />
+        <Cardgame />
       </div>
 
 
